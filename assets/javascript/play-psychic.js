b9275d60efb2712ac8ede5dd1a104e050900f94c
alex nichols <alex nichols>
2017-02-01 20:48:43 -0800

    game functions finalized

--- assets/javascript/play-psychic.js
+++ assets/javascript/play-psychic.js
@@ -2,7 +2,22 @@
 var guesses=[];
 var guessedLetter=[];
 var reset=false;
+var maxGuesses= 10;
+document.getElementById("maxGuess").innerHTML = maxGuesses;
+var wins= 0;
+document.getElementById("wins");
+var losses= 0;
+document.getElementById("losses");
 var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+function startOver() {
+
+	guessedLetter=[];
+	guesses=[];
+	maxGuesses=10;
+	computerIndex= Math.floor(Math.random() * choices.length);
+	computersChoice= choices[computerIndex];
+}
 
 //listen for event onkeydown
 window.onkeydown = function(event) {
@@ -22,20 +37,40 @@ window.onkeydown = function(event) {
 	 		console.log("first press");
 
 		 	//generate a random letter
-			var computersChoice = Math.floor(Math.random() * choices.length);
+			var computerIndex = Math.floor(Math.random() * choices.length);
+
+			var computersChoice = choices[computerIndex];
 
 			//display users guess
 			var userGuess = document.getElementById("user-guess");
 
-			document.onkeyup = function() {
 			userGuess.textContent = guesses;
-			}
+			
 
 			if(usersChoice !== computersChoice) {
-
+				
+				maxGuesses--;
+				document.getElementById("maxGuess").innerHTML = maxGuesses;
 
 			}
+			else {
+
+				wins++;
+				document.getElementById("wins").innerHTML = wins;
+				startOver();
+			}
+
+			if(maxGuesses < 1) {
+
+				losses++;
+				document.getElementById("losses").innerHTML = losses;
+				startOver();
+			}
+
+
+			
 	 	}
 	 
+
 }
 
